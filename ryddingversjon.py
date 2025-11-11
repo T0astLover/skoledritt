5642947bbc20365e6b91af8abecc526b4a6495b5
toastlover <95256956+T0astLover@users.noreply.github.com>
2025-11-11 23:10:27 +0100

    ryddig versjon
    uten input i console

---
+++ ryddingversjon.py
@@ -0,0 +1,50 @@
+import os 
+import platform
+
+#henter opperativsystemet til brukern og brukernavn
+try: 
+    systemet = os.name
+    osVersjon = platform.version()
+except Exception as fkode:
+    print("feil ved henting av OS", fkode)
+#posix = linux/mac #NT = windows 
+
+#måtte få hjelp av AI til denne delen men skal klare og forklare det :) 
+import shutil 
+try: 
+    storage = f"{shutil.disk_usage('C:\\').free / (1024**3):.2f} GB"
+except Exception as fkode:
+    print("feil ved henting av lagring", fkode)
+
+#brukernavn
+try:
+    username = os.getlogin()
+except Exception as fkode:
+    print("feil ved henting av brukernavn", fkode)
+
+#ip adreesse
+try: 
+    ipaddresse = os.popen("ipconfig").read()
+except Exception as fkode:
+    print("feil ved henting av ip adresse", fkode)
+
+#apper
+try:
+    apper = os.popen("wmic product get name,version").read()
+except Exception as fkode:	
+    print("feil ved henting av apper", fkode)
+
+#lager filen med brukernavet
+filsti = f"{username}.txt"
+
+#skriver in infoen til filen
+try:
+    with open(filsti, "w") as fil:
+        fil.write(f"Operativsystem: {systemet} {osVersjon}\n")
+        fil.write(f"Brukernavn: {username}\n")
+        fil.write(f"lagring: {storage}\n")
+        fil.write(f"IP adresse: {ipaddresse}\n")
+        fil.write(f"Installerte apper: {apper}\n")
+    print(f"Informasjon lagret i filen: {filsti}")
+except Exception as fkode:
+    print("feil ved lagring av fil", fkode)
